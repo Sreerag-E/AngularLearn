cfbcf0bdc5f2b6c33f4f408f8140b35ff3549175
E <jerry.joy@kcc.com>
2019-12-31 15:49:07 +0530

    latest app changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule } from '@angular/forms'
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
- 
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([]),
+    AppRoutingModule,
     FormsModule
   ],
   providers: [],
